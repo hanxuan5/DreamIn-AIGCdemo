93a40fbdc3a918857c32d7ed76d7b6f3e8123c89
Chufan Wu <wuchufan6@gmail.com>
2023-03-08 01:18:22 -0500

    *Player animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private float _speed = 100.0f;
+    Animator animator;
+    
+    // Initialize Animator
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
+
+        bool up = v > 0;
+        bool down = v < 0;
+        bool right = h > 0;
+        bool left = h < 0;
+
+        animator.SetBool("up", up);
+        animator.SetBool("down", down);
+        animator.SetBool("right", right);
+        animator.SetBool("left", left);
 
         h *= Mathf.Sqrt(1 - (v * v) / 2.0f);
         v *= Mathf.Sqrt(1 - (h * h) / 2.0f);
